2ac23a457463bd5dee6b411b56a9edf05ff4f652
muhsayd <muhsayd@hotmail.com>
2017-08-26 15:22:54 +0300

    Switch exiqgrep and exim to apslute path

--- track-spam.sh
+++ track-spam.sh
@@ -1,13 +1,13 @@
-for msgid in `exiqgrep -i`; 
+for msgid in `/usr/sbin/exiqgrep -i`; 
 do
 	if ! $( grep -q $msgid /root/spam-summary.txt );
 	then
-		subject=`exim -Mvh $msgid | egrep 'Subject:' | cut -d: -f2-`; 
-		ip=`exim -Mvh $msgid | egrep 'Received: from' | awk -F'[' '{print $2}' | awk -F']' '{print $1}'`; 
-		helo=`exim -Mvh $msgid | egrep 'Received: from' | awk -F'helo=' '{print $2}' | awk -F')' '{print $1}'`; 
-		reciept=`exim -Mvh $msgid | egrep ' To: ' | awk -F' To: ' '{print $2}'`; 
-		sender=`exim -Mvh $msgid | egrep ' From: ' | awk -F' From: ' '{print $2}' | awk -F'<' '{print $2}' | awk -F'>' '{print $1}'`; 
-		script=`exim -Mvh $msgid | egrep ' X-PHP-Script: ' | awk -F' X-PHP-Script: ' '{print $2}' | awk -F ' for ' '{print $1}'`;
+		subject=`/usr/sbin/exim -Mvh $msgid | egrep 'Subject:' | cut -d: -f2-`; 
+		ip=`/usr/sbin/exim -Mvh $msgid | egrep 'Received: from' | awk -F'[' '{print $2}' | awk -F']' '{print $1}'`; 
+		helo=`/usr/sbin/exim -Mvh $msgid | egrep 'Received: from' | awk -F'helo=' '{print $2}' | awk -F')' '{print $1}'`; 
+		reciept=`/usr/sbin/exim -Mvh $msgid | egrep ' To: ' | awk -F' To: ' '{print $2}'`; 
+		sender=`/usr/sbin/exim -Mvh $msgid | egrep ' From: ' | awk -F' From: ' '{print $2}' | awk -F'<' '{print $2}' | awk -F'>' '{print $1}'`; 
+		script=`/usr/sbin/exim -Mvh $msgid | egrep ' X-PHP-Script: ' | awk -F' X-PHP-Script: ' '{print $2}' | awk -F ' for ' '{print $1}'`;
 		echo "MSGID:$msgid; Sender:$sender; ReCeipt:$reciept; Script:$script; IP:$ip; Helo:$helo; Subject:$subject" >> /root/spam-summary.txt
 	fi
 	done
