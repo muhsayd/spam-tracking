5af8b67fd20416b7e25c5fdd06d02b92664f0072
muhsayd <muhsayd@hotmail.com>
2017-08-26 13:12:27 +0200

    fix the summary file path

---
+++ install.sh
@@ -0,0 +1,8 @@
+if [ -e "/root/spam-tracking/" ]; then
+	cd /root/spam-tracking/
+	git pull https://github.com/muhsayd/spam-tracking.git >/dev/null
+else
+	cd /root
+	git clone https://github.com/muhsayd/spam-tracking.git >/dev/null
+fi
+grep -q '/root/spam-tracking/track-spam.sh'  /var/spool/cron/root || echo "*	* * * * sh /root/spam-tracking/track-spam.sh" >> /var/spool/cron/root
